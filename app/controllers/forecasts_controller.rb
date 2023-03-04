1934f228fc137c666410b0adf24c2ef457e49e7b
Knnav <kznnav@gmail.com>
2023-03-04 18:47:38 -0300

    Add more functional data to views

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -7,12 +7,16 @@ class ForecastsController < ApplicationController
       @forecast = Creators::ForecastService.call(params['iata'], current_user)
 
       if @forecast.present?
-        redirect_to forecast_path(@forecast)
+        redirect_to "/forecast/#{@forecast.id}"
       end
     end
   end
 
   def show
     @forecast = Forecast.find(params[:id])
+
+    if user_signed_in?
+      @past_forecasts = current_user.forecasts.last(11).first(10)
+    end
   end
 end
