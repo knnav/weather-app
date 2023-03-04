fa97feb09b2c4531c780612d201d90cb54963884
Knnav <kznnav@gmail.com>
2023-03-04 20:08:31 -0300

    Add helpers for data presentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def formatted_weather(forecast)
+    "#{forecast.celsius_temp}°C / #{forecast.fahrenheit_temp}°F"
+  end
+
+  def city_display_name(city)
+    "#{city.name}, #{city.country.name} (#{city.iata.upcase})"
+  end
 end
