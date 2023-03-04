127f4052d928c110d086d4a39b5c277d6f76f73b
Knnav <kznnav@gmail.com>
2023-03-04 16:41:02 -0300

    Add country and region fields to City model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_011004) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_061931) do
   create_table "cities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.float "latitude"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_011004) do
     t.string "iata"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "region"
+    t.string "country"
   end
 
   create_table "forecasts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
