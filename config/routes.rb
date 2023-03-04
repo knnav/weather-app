84d7b1a97f31fd0592270ceb1d4bfc7025a024ef
Knnav <kznnav@gmail.com>
2023-03-04 02:14:21 -0300

    Add routes for forecasts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'forecasts#new'
   devise_for :users
+
+  resource :forecast, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
